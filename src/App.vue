49322e65293e3208be6b80b312f52df72076de67
Melania Rizzuto <melrizzuto4@gmail.com>
2025-06-18 15:23:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import LearningResource from './components/learningResource/LearningResource.vue';
+import './style/app.css';
+const storedResources = [
+  {
+    id: 'official-guide',
+    title: 'Official Guide',
+    description: 'The official Vue.js documentation and guide.',
+    link: 'https://vuejs.org/guide/introduction.html',
+  },
+  {
+    id: 'google',
+    title: 'Google',
+    description: 'Search the web with Google.',
+    link: 'https://www.google.com',
+  },
+];
+</script>
+
+<template>
+  <ul>
+    <LearningResource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :resource="resource"
+    />
+  </ul>
+</template>
---
+++ src/components/learningResource/LearningResource.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineProps } from 'vue';
+import '../../style/learningResource.css';
+const props = defineProps({
+  resource: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ props.resource.title }}</h3>
+        <button>Delete</button>
+      </header>
+      <p>{{ props.resource.description }}</p>
+      <nav>
+        <a :href="props.resource.link" target="_blank">View Resource</a>
+      </nav>
+    </div>
+  </li>
+</template>
